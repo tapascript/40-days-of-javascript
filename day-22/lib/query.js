9574ba1c7ed9a7b61695161d29c3731caad5cd76
Tapas Adhikary <tapas.adhikary@gmail.com>
2025-04-30 10:25:29 +0530

    (feat) Day 22 - code doc

--- day-22/lib/query.js
+++ day-22/lib/query.js
@@ -1,3 +1,4 @@
+// You no need to learn this method now. We will leaern it deeper in the future sessions.
 function query(endpoint, callback, options) {
     fetch(`http://localhost:3000/${endpoint}`, options)
         .then(function (response) {
