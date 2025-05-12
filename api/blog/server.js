893242915250b087e2dcb69d9689189c6dbebe61
Tapas Adhikary <tapas.adhikary@gmail.com>
2025-05-12 12:24:48 +0530

    (feat) Day 25 - The fetch() API

---
+++ api/blog/server.js
@@ -0,0 +1,26 @@
+const jsonServer = require("json-server");
+const fs = require("fs");
+const path = require("path");
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+// ✅ POST /api/order → create a new order
+server.post("/login", (req, res) => {
+    console.log("Headers:", req.headers);
+    console.log("Body:", req.body);
+
+    res.json({
+        message: "LoggedIn successfully!"
+    });
+});
+
+server.use(router);
+
+server.listen(3000, () => {
+    console.log("✅ JSON Server running at http://localhost:3000");
+});
